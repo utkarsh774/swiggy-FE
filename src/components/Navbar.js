57284ceb353f27f37e13cbb32e71e345e87ae194
Utkarsh Singh <arsh2502singh@gmail.com>
2025-03-08 00:19:58 +0530

    Navbar with logo and utils folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react"
-
+import { appLogo } from "../utils/constants"
 const Navbar = () =>
 {
     return (
         <div>
 
+
       <nav className="flex justify-between bg-gray-500 p-4 text-black">
         {/* first child */}
-        <div>Logo</div>
+        <img src = {appLogo}  className="w-16 h-12"/>
        {/*  second child */}
         <ul className="flex space-x-6">
         <li>home</li>
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const appLogo = "https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png";
