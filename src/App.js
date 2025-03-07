6cbe7d752f1b4efb0b0475b97b3344b02c5ab2b1
Utkarsh Singh <arsh2502singh@gmail.com>
2025-03-08 02:52:14 +0530

    routing on url done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Cart from "./components/About"
+import About from "./components/About"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+
 function App() {
   return (
     <>
-   <Navbar/>
+    <Router>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/cart" element={<Cart></Cart>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+   {/* <Route path="*" element={<About></About>}></Route> */}
+      <Route></Route>
+    </Routes>
+    </Router>
    </>
   );
 }
